62656a196fe83ded01007f3f9fb08117c2c01406
AlexanderTabakov <sir.alexander.tabakov@gmail.com>
2024-02-13 14:46:29 +0700

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import {App} from "app/App";
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from "react-router-dom";
+import TestMobx from "components/TestMobx";
 
 const root = (
     <BrowserRouter>
-        <App />
+        <TestMobx />
     </BrowserRouter>
 )
 
